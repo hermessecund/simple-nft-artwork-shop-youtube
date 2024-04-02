4edf49c12c7c08f6a5d368e7bd079c0979007266
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-02 02:58:47 +0000

    Update constants.ts

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,5 +1,5 @@
-export const NFT_CONTRACT_ADDRESS = "YOUR_CONTRAT_ADDRESS";
+export const NFT_CONTRACT_ADDRESS = "0x2be50979b6796C63fBA632F315466979b4740F8e";
 
 export const NFT_CHECKOUT_LINKS = [
-    "YOUR_CHECKOUT_LINKS",
+    "https://payments.thirdweb.com/checkout/0e4c049b-67b9-422f-befe-b6ec16710fb4",
 ];
